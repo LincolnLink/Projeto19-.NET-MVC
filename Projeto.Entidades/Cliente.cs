625f01833de37190f6a4bc46e72f07809c61b87e
Lincoln Ferreira Campos <link7-7@hotmail.com>
2017-10-29 15:36:01 -0200

    Modelando a entidade!

---
+++ Projeto.Entidades/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataHoraCadastro { get; set; }
+
+        public Cliente()
+        {
+
+        }
+        public Cliente(int idCliente, string nome, string email, DateTime dataHoraCadastro)
+        {
+            IdCliente = idCliente;
+            Nome = nome;
+            Email = email;
+            DataHoraCadastro = dataHoraCadastro;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {IdCliente}, Nome: {Nome}, Email: {Email}," +
+                $"Data e Hora do Cadastro: {DataHoraCadastro}";
+        }
+    }
+}
